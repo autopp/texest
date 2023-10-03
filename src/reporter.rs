bffef23c2c0e0cffee36564579dbd70c0c00f694
autopp <autopp.inc@gmail.com>
2023-10-04 00:03:05 +0900

    feat: report io error

--- src/reporter.rs
+++ src/reporter.rs
@@ -5,13 +5,19 @@ use std::io::Write;
 use crate::test_case::{TestCase, TestResult};
 
 pub enum Color {
+    #[allow(dead_code)]
     Black,
     Red,
     Green,
+    #[allow(dead_code)]
     Yellow,
+    #[allow(dead_code)]
     Blue,
+    #[allow(dead_code)]
     Magenta,
+    #[allow(dead_code)]
     Cyan,
+    #[allow(dead_code)]
     White,
     Reset,
 }
@@ -33,25 +39,25 @@ impl Color {
 }
 
 pub trait Formatter {
-    fn on_run_start(&mut self, w: &mut Box<dyn Write>, cm: &ColorMarker);
+    fn on_run_start(&mut self, w: &mut Box<dyn Write>, cm: &ColorMarker) -> Result<(), String>;
     fn on_test_case_start(
         &mut self,
         w: &mut Box<dyn Write>,
         cm: &ColorMarker,
         test_case: &TestCase,
-    );
+    ) -> Result<(), String>;
     fn on_test_case_end(
         &mut self,
         w: &mut Box<dyn Write>,
         cm: &ColorMarker,
         test_result: &TestResult,
-    );
+    ) -> Result<(), String>;
     fn on_run_end(
         &mut self,
         w: &mut Box<dyn Write>,
         cm: &ColorMarker,
         test_results: &Vec<TestResult>,
-    );
+    ) -> Result<(), String>;
 }
 
 pub struct Reporter<'a, 'b> {
@@ -82,6 +88,7 @@ impl ColorMarker {
         }
     }
 
+    #[allow(dead_code)]
     pub fn black<S: AsRef<str>>(&self, s: S) -> String {
         self.wrap(Color::Black, s)
     }
@@ -94,26 +101,32 @@ impl ColorMarker {
         self.wrap(Color::Green, s)
     }
 
+    #[allow(dead_code)]
     pub fn yellow<S: AsRef<str>>(&self, s: S) -> String {
         self.wrap(Color::Yellow, s)
     }
 
+    #[allow(dead_code)]
     pub fn blue<S: AsRef<str>>(&self, s: S) -> String {
         self.wrap(Color::Blue, s)
     }
 
+    #[allow(dead_code)]
     pub fn magenta<S: AsRef<str>>(&self, s: S) -> String {
         self.wrap(Color::Magenta, s)
     }
 
+    #[allow(dead_code)]
     pub fn cyan<S: AsRef<str>>(&self, s: S) -> String {
         self.wrap(Color::Cyan, s)
     }
 
+    #[allow(dead_code)]
     pub fn white<S: AsRef<str>>(&self, s: S) -> String {
         self.wrap(Color::White, s)
     }
 
+    #[allow(dead_code)]
     pub fn reset<S: AsRef<str>>(&self, s: S) -> String {
         self.wrap(Color::Reset, s)
     }
@@ -132,24 +145,24 @@ impl<'a, 'b> Reporter<'a, 'b> {
         }
     }
 
-    pub fn on_run_start(&mut self) {
+    pub fn on_run_start(&mut self) -> Result<(), String> {
         let cm = ColorMarker::new(self.use_color);
-        self.formatter.on_run_start(self.w, &cm);
+        self.formatter.on_run_start(self.w, &cm)
     }
 
-    pub fn on_test_case_start(&mut self, test_case: &TestCase) {
+    pub fn on_test_case_start(&mut self, test_case: &TestCase) -> Result<(), String> {
         let cm = ColorMarker::new(self.use_color);
-        self.formatter.on_test_case_start(self.w, &cm, test_case);
+        self.formatter.on_test_case_start(self.w, &cm, test_case)
     }
 
-    pub fn on_test_case_end(&mut self, test_result: &TestResult) {
+    pub fn on_test_case_end(&mut self, test_result: &TestResult) -> Result<(), String> {
         let cm = ColorMarker::new(self.use_color);
-        self.formatter.on_test_case_end(self.w, &cm, test_result);
+        self.formatter.on_test_case_end(self.w, &cm, test_result)
     }
 
-    pub fn on_run_end(&mut self, test_results: &Vec<TestResult>) {
+    pub fn on_run_end(&mut self, test_results: &Vec<TestResult>) -> Result<(), String> {
         let cm = ColorMarker::new(self.use_color);
-        self.formatter.on_run_end(self.w, &cm, test_results);
+        self.formatter.on_run_end(self.w, &cm, test_results)
     }
 }
 
--- src/reporter/simple_formatter.rs
+++ src/reporter/simple_formatter.rs
@@ -3,14 +3,21 @@ use super::Formatter;
 pub struct SimpleReporter {}
 
 impl Formatter for SimpleReporter {
-    fn on_run_start(&mut self, _w: &mut Box<dyn std::io::Write>, _cm: &super::ColorMarker) {}
+    fn on_run_start(
+        &mut self,
+        _w: &mut Box<dyn std::io::Write>,
+        _cm: &super::ColorMarker,
+    ) -> Result<(), String> {
+        Ok(())
+    }
 
     fn on_test_case_start(
         &mut self,
         _w: &mut Box<dyn std::io::Write>,
         _cm: &super::ColorMarker,
         _test_case: &crate::test_case::TestCase,
-    ) {
+    ) -> Result<(), String> {
+        Ok(())
     }
 
     fn on_test_case_end(
@@ -18,12 +25,13 @@ impl Formatter for SimpleReporter {
         w: &mut Box<dyn std::io::Write>,
         cm: &super::ColorMarker,
         test_result: &crate::test_case::TestResult,
-    ) {
+    ) -> Result<(), String> {
         if test_result.is_passed() {
-            write!(w, "{}", cm.green("."));
+            write!(w, "{}", cm.green("."))
         } else {
-            write!(w, "{}", cm.red("F"));
+            write!(w, "{}", cm.red("F"))
         }
+        .map_err(|err| err.to_string())
     }
 
     fn on_run_end(
@@ -31,7 +39,7 @@ impl Formatter for SimpleReporter {
         w: &mut Box<dyn std::io::Write>,
         _cm: &super::ColorMarker,
         test_results: &Vec<crate::test_case::TestResult>,
-    ) {
+    ) -> Result<(), String> {
         let failed_count = test_results
             .iter()
             .filter(|test_result| !test_result.is_passed())
@@ -42,6 +50,7 @@ impl Formatter for SimpleReporter {
             "\n{} test cases, {} failures\n",
             test_results.len(),
             failed_count
-        );
+        )
+        .map_err(|err| err.to_string())
     }
 }
