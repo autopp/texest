eb37aeecfa0e5c71614a71b50def981f74603a19
autopp <autopp.inc@gmail.com>
2024-06-20 22:42:12 +0900

    feat: add WaitCondition::parse

--- src/test_case/wait_condition.rs
+++ src/test_case/wait_condition.rs
@@ -3,6 +3,9 @@ mod sleep;
 
 use std::time::Duration;
 
+use crate::ast::Map;
+use crate::validator::Validator;
+
 pub use self::http::HttpCondition;
 pub use self::sleep::SleepCondition;
 
@@ -19,6 +22,19 @@ impl WaitCondition {
             WaitCondition::Http(http_condition) => http_condition.wait().await,
         }
     }
+
+    pub fn parse(v: &mut Validator, name: &str, params: &Map) -> Option<Self> {
+        match name {
+            "sleep" => SleepCondition::parse(v, params).map(WaitCondition::Sleep),
+            "http" => HttpCondition::parse(v, params).map(WaitCondition::Http),
+            _ => {
+                v.in_field("type", |v| {
+                    v.add_violation(format!("\"{}\" is not valid wait condition type", name))
+                });
+                None
+            }
+        }
+    }
 }
 
 impl Default for WaitCondition {
@@ -26,5 +42,57 @@ impl Default for WaitCondition {
         WaitCondition::Sleep(SleepCondition {
             duration: Duration::from_secs(0),
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::validator::testutil;
+
+    use super::*;
+    use indexmap::{indexmap, IndexMap};
+    use pretty_assertions::assert_eq;
+    use rstest::rstest;
+    use serde_yaml::Value;
+
+    #[rstest]
+    #[case("with sleep", "sleep", indexmap! { "duration" => Value::from("1s") }, Some(WaitCondition::Sleep(SleepCondition { duration: Duration::from_secs(1) })), vec![])]
+    #[case("with http", "http", indexmap! {
+            "port" => Value::from(8080),
+            "path" => Value::from("/health"),
+        }, Some(WaitCondition::Http(HttpCondition {
+            port: 8080,
+            path: "/health".to_string(),
+            initial_delay: Duration::from_secs(0),
+            interval: Duration::from_secs(0),
+            max_retry: 3,
+            timeout: Duration::from_secs(1),
+        })), vec![])]
+    #[case("with unknown wait condition", "unknown", indexmap! {}, None, vec![(".type", "\"unknown\" is not valid wait condition type")])]
+    fn parse(
+        #[case] title: &str,
+        #[case] name: &str,
+        #[case] params: IndexMap<&str, Value>,
+        #[case] expected_value: Option<WaitCondition>,
+        #[case] expected_violation: Vec<(&str, &str)>,
+    ) {
+        let (mut v, violation) = testutil::new_validator();
+
+        assert_eq!(
+            expected_value,
+            WaitCondition::parse(&mut v, name, &params.iter().map(|(k, v)| (*k, v)).collect()),
+            "{}",
+            title
+        );
+
+        assert_eq!(
+            expected_violation
+                .iter()
+                .map(|(path, msg)| violation(path, msg))
+                .collect::<Vec<_>>(),
+            v.violations,
+            "{}",
+            title
+        )
     }
 }
