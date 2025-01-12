c968168de3ba942f0bbd2c0ea9ad04a846d1bd6a
autopp <autopp.inc@gmail.com>
2025-01-12 22:23:46 +0900

    test: make e2e test to be able to specify file and option

--- e2e/run.sh
+++ e2e/run.sh
@@ -6,10 +6,13 @@ my_dir=$(cd $(dirname $0); pwd)
 target_cmd=${E2E_TARGET:-${my_dir}/../target/debug/texest}
 tester_cmd="${E2E_TESTER:-texest}"
 
+options=${OPTIONS:-}
+files=${FILES:-*.yaml}
+
 have_error=no
-for file in $my_dir/cases/*.yaml; do
+for file in $my_dir/cases/${files}; do
   echo $(basename ${file})
-  TEXEST="${target_cmd}" "${tester_cmd}" "${file}" || have_error=yes
+  TEXEST="${target_cmd}" "${tester_cmd}" ${options} "${file}" || have_error=yes
   echo
 done
 
